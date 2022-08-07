0f199c93006b79c34c4498b535dae927369182f5
Anslie <brantae@gmail.com>
2022-08-06 20:45:54 -0500

    commit tree 134007fece2eff0aed2ffa8af93f672fd7f0047b

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I\'ve been clicked');
+    });
 }
+
+
+/* function addingEventListener() {
+    document.querySelector('button').addEventListener('click', () => alert('CLICK'))
+    };
+*/
+    addingEventListener();
